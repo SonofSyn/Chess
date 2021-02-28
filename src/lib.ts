21919f98647c616c7da2f1b4057373368f436a5c
SonofSyn <Kai.Bissell@gmx.de>
2021-02-28 16:51:00 +0100

    Update package

--- src/lib.ts
+++ src/lib.ts
@@ -2,4 +2,4 @@
 export * from './game/mechanics/execMove'
 export * from './game/game'
 export * from './game/board'
-export * from './types/interfaces'
+export * from './game/moves/determinPossibleMoves'
